713e4e0ba83cc950fc2ea307fec34548fd8fc925
Rhys Powell <rhys@rhyspowell.com>
2014-09-22 22:02:41 +0100

    finally a working pagination. Still requires posts per page to be found via the config file

--- blogger/blog/views.py
+++ blogger/blog/views.py
@@ -4,22 +4,23 @@ from flask.ext.login import current_user, login_required
 from blogger.data import query_to_list, db
 from blogger.admin.models import Entries
 
-POSTS_PER_PAGE = 2
+POSTS_PER_PAGE = 5
 blogger = Blueprint("blogger", __name__, static_folder='static')
 
 
 #main page route
 @blogger.route('/')
 @blogger.route('/<int:page>')
-def show_entries(page = 1):
+def show_entries(page=1):
     #cur = g.db.execute('select title, text from entries order by id desc')
     #entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     #cur = g.db.execute('select name, url from menu')
     #menu_items = [dict(name=row[0], url=row[1]) for row in cur.fetchall()]
     #entries = Entries.query.paginate(page, POSTS_PER_PAGE, False)
-    entries = Entries.query.all()
+    #entries = Entries.query.all()
     #posts = entries.posts.paginate(page, POSTS_PER_PAGE, False)
-    #entries = Entries.query.paginate(page, posts_per_page, False).items
+    pages = Entries.query.paginate(page, POSTS_PER_PAGE, False)
+    entries = Entries.query.paginate(page, POSTS_PER_PAGE, False).items
     #menu_items = Menu_Items.query.all()
     #data = query_to_list(entries)
     #results = []
@@ -38,4 +39,4 @@ def show_entries(page = 1):
 
     #return render_template("tracking/sites.html", sites=results, form=form)
     #return render_template('show_entries.html', entries=entries, menu_items=menu_items)
-    return render_template('show_entries.html', entries=entries)
+    return render_template('show_entries.html', entries=entries, pages=pages)
