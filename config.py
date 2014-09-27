2f0e3eddd743937a295d5c6f1c29ae45f88a9770
Rhys Powell <rhys@rhyspowell.com>
2014-09-27 22:04:46 +0100

    removed the openshift database configuration

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 #DATABASE='blogger.db'
 
 SQLALCHEMY_DATABASE_URI='mysql://root:password@127.0.0.1/blogger'
-#SQLALCHEMY_DATABASE_URI='mysql://admin2WzA6ZS:93Pg4DJc8nfr@$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/blog'
+
 
 #Debug information
 DEBUG=True
