e9b242349d4f3ada0cece6a4ad87dcb59d81f1fa
Rhys Powell <rhys@rhyspowell.com>
2014-09-27 22:00:18 +0100

    fixed the showing single post click through

--- blogger/blog/views.py
+++ blogger/blog/views.py
@@ -7,14 +7,24 @@ from blogger.admin.models import Entries, MenuItems
 
 POSTS_PER_PAGE = 5
 blogger = Blueprint("blogger", __name__, static_folder='static')
+TITLE = 'The Random Randoms'
 
 
 #main page route
 @blogger.route('/')
 @blogger.route('/<int:page>')
 def show_entries(page=1):
+    title = TITLE
     pages = Entries.query.paginate(page, POSTS_PER_PAGE, False)
     entries = Entries.query.filter_by(status = 1).order_by(Entries.publishedtime.desc()).paginate(page, POSTS_PER_PAGE, False).items
     menuitems = MenuItems.query.all()
 
-    return render_template('show_entries.html', entries=entries, pages=pages, menuitems=menuitems)
+    return render_template('show_entries.html', entries=entries, pages=pages, menuitems=menuitems, title=title)
+
+@blogger.route('/<posttitle>')
+def showpost(posttitle):
+    title = TITLE
+    entries = Entries.query.filter_by(title = posttitle)
+    menuitems = MenuItems.query.all()
+
+    return render_template('show_entries.html', entries=entries, title=title, pages=1, menuitems=menuitems )
--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 #Database information
 #DATABASE='blogger.db'
 
-#SQLALCHEMY_DATABASE_URI='mysql://root:password@127.0.0.1/blogger'
-SQLALCHEMY_DATABASE_URI='mysql://admin2WzA6ZS:93Pg4DJc8nfr@$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/blog'
+SQLALCHEMY_DATABASE_URI='mysql://root:password@127.0.0.1/blogger'
+#SQLALCHEMY_DATABASE_URI='mysql://admin2WzA6ZS:93Pg4DJc8nfr@$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/blog'
 
 #Debug information
 DEBUG=True
